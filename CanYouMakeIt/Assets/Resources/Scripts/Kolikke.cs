af61ae692f5073e91a4362e9d2db8d7cbc9b2545
Toni <a1503035@myy.haaga-helia.fi>
2018-02-25 20:28:36 +0200

    rahan laskuri

--- CanYouMakeIt/Assets/Resources/Scripts/Kolikke.cs
+++ CanYouMakeIt/Assets/Resources/Scripts/Kolikke.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Kolikke : MonoBehaviour {
+	public Text money;
+	public static int currentMoney = 0;
 
 	void OnTriggerEnter2D (Collider2D col) {
-
-		Destroy (gameObject); }
+		currentMoney = currentMoney + 1;
+		money.text = currentMoney.ToString();
+		Destroy (gameObject); 
+	}
 }
